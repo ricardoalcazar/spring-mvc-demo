8e46f315449b73b460dd1f9b9ce4e4db847e96cf
Ricardo Alcazar <ricardo.alcazar@chamberlain.com>
2022-04-07 08:59:52 -0500

    [ADD] Initial commit

---
+++ src/test/java/com/alcazar/spring/mvc/demo/SpringMvcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alcazar.spring.mvc.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
